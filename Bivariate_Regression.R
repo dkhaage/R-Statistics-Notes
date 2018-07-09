f2e11b7f9bf3a65778c6109ad68b2c69c2a2cc10
Dieter K Haage <34729105+dkhaage@users.noreply.github.com>
2018-07-09 16:30:52 -0500

    Add files via upload

---
+++ Bivariate_Regression.R
@@ -0,0 +1,37 @@
+# Ex07_02
+# Computing a bivariate regression
+
+# Load data
+?trees
+data(trees)
+trees[1:5,] # Show first 5 lines
+
+# Quick graphical check on the data
+hist(trees$Height)
+hist(trees$Girth)
+plot(trees$Girth, trees$Height)
+abline(lm(trees$Height ~ trees$Girth))
+
+# Linear regression model
+# NOTE: Correlation is used to test the strength of the association
+# Regression allows you to PREDICT values based on another variable
+reg1 <- lm(Height ~ Girth, data = trees)
+reg1
+summary(reg1)
+
+# Confidence intervals for coefficients
+confint(reg1)
+
+# Predict values based on regression equation
+predict(reg1) # Predicted height based on girth
+predict(reg1, interval = "prediction") # CI for predicted height
+
+# Regression diagnostics
+lm.influence(reg1)
+influence.measures(reg1)
+
+rm(list = ls())
+
+# Basics of linear regression.
+# One variable predicting an outcome on a second quantitative variable
+# One of the basic measures of associations in statistics
