e214bbc90e0ac31f1571b8f321ef9365e1569df2
Dieter K Haage <34729105+dkhaage@users.noreply.github.com>
2018-07-13 12:13:02 -0500

    Add files via upload

---
+++ Compare_Paired_Means_PairedTtest.R
@@ -0,0 +1,42 @@
+# Ex07_04
+# Comparing paired means: Paired t-test
+# Used to test after an intervention
+
+
+
+# Load Data
+# Create random data
+t1 <- rnorm(50, mean = 52, sd = 6) # Time 1
+dif <- rnorm(50, mean = 6, sd = 12) # Difference
+t2 <- t1 + dif # Time 2
+
+# SOme quick plots to check data
+hist(t1)
+hist(dif)
+hist(t2)
+boxplot(t1, t2)
+
+# Save variables in dataframe and use "MASS"
+# to create parallel coordinate plot
+pairs <- data.frame(t1, t2)
+require("MASS")
+parcoord(pairs, var.label = TRUE)
+
+# Paird t-test (with defaults)
+t.test(t2, t1, paired = TRUE)
+
+# Paired t-test with options
+t.test(t2, t1,
+       paired = TRUE,
+       mu = 6, # Specify non-0 null value
+       alternative = "greater", # One-tailed test
+       conf.level = 0.99) # 99% CI (vs. 95%)
+
+# Clean up
+detach("package:MASS", unload=TRUE)
+rm(list = ls())
+
+
+
+
+
